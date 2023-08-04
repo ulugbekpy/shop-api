717f8aa4b6e0ad8ae18d711ff809b80261728e84
ulugbekpy <ulugbekpycpp@gmail.com>
2023-08-04 15:28:19 +0500

    removed unnecessary lines

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,5 @@
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.urls import re_path
-from .views import ProductViewSet, CollectionViewSet
-
 from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CollectionViewSet
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
